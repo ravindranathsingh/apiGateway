fefcebf3d2e27262fdd0d846041264644aaeec31
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-19 16:26:49 +0530

    Implemented API gateway

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const axios = require('axios')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
@@ -11,7 +13,37 @@ const serverSetUp = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
 
+    const limiter = rateLimit({
+        windowMs: 2*60*1000,
+        max: 5
+    })
+
     app.use(morgan('combined'))
+    app.use(limiter)
+    app.use('/bookingservice', async (req, res, next ) => {
+        console.log(req.headers['x-axis-token'])
+        try {
+            const response = await axios.get('http://localhost:3001/api/v1/isauthenticated', {
+                headers: {
+                    'x-axis-token': req.headers['x-axis-token']
+                }
+            })
+            console.log(response.data)
+            console.log('Hi')
+            if (response.data.success) {
+                next();
+            } else {
+                return res.status(401).json({
+                    message: 'unauthorised'
+                })
+            } 
+        } catch (error) {
+            return res.status(401).json({
+                message: 'unauthorised'
+            })
+        }
+               
+    })
     app.use('/bookingservice', createProxyMiddleware({target: 'http://localhost:3002/', changeOrigin: true}))
     app.get('/home', ( req, res ) => {
         return res.json({message: 'OK'})
